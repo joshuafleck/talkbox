ccf4f57318242240db9f134431c17d78c27b61e8
Joshua Fleck <joshua.fleck@simplybusiness.co.uk>
2017-05-14 15:31:20 +0100

    Initialized voice app

---
+++ apps/voice/lib/voice.ex
@@ -0,0 +1,18 @@
+defmodule Voice do
+  @moduledoc """
+  Documentation for Voice.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Voice.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/voice/test/voice_test.exs
@@ -0,0 +1,8 @@
+defmodule VoiceTest do
+  use ExUnit.Case
+  doctest Voice
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
