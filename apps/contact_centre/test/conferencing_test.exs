d9c32b3a4f9ad0ec7427f1114c972901081fefe4
Joshua Fleck <joshua.fleck@simplybusiness.co.uk>
2017-08-07 12:06:07 +0100

    Compare actual and expected events
    Ignoring the events that are published externally

--- apps/contact_centre/test/conferencing_test.exs
+++ apps/contact_centre/test/conferencing_test.exs
@@ -15,59 +15,35 @@ defmodule ContactCentre.ConferencingTest do
     assert_recorded_event_logs_match_actual(context)
   end
 
-  @events_published_without [
+  @events_published_externally [
     Events.CallFailedToJoinConference,
     Events.CallJoinedConference,
     Events.CallLeftConference,
     Events.CallRequestFailed,
     Events.CallStatusChanged,
     Events.ChairpersonRequestsToRemoveCall,
+    Events.ConferenceEnded,
     Events.UserRequestsCall]
   defp assert_recorded_event_logs_match_actual(context) do
     event_logs_path = "test/conferencing_test_event_logs/#{context.test}"
-    unexpected_events = event_logs_path
+
+    event_logs_path
     |> Events.Persistence.read()
-    |> Enum.concat([nil]) # Indicates the last item
-    |> Enum.with_index()
-    |> Enum.map(&publish_external_and_wait_for_expected_event(&1))
-    |> Enum.filter(&event_unexpected?(&1))
+    |> Enum.filter(&external_event?(&1))
+    |> Enum.map(&Events.publish(&1))
 
-    if Enum.any?(unexpected_events) do
-      first_unexpected_event = Enum.at(unexpected_events, 0)
-      assert elem(first_unexpected_event, 2) == elem(first_unexpected_event, 3)
-    end
+    expected_internal_events = event_logs_path
+    |> Events.Persistence.read()
+    |> Enum.reject(&external_event?(&1))
+
+    :timer.sleep(100) # Give the consumer time to process
+    actual_internal_events = Events.Persistence.published
+    |> Enum.reject(&external_event?(&1))
+
+    assert expected_internal_events == actual_internal_events
   end
 
-  defp publish_external_and_wait_for_expected_event({event, index}) do
-    if event != nil && Enum.member?(@events_published_without, event.__struct__) do
-      Events.publish(event)
-    end
-    wait_for_expected_event(event, index)
-  end
-
-  defp wait_for_expected_event(event, index, attempts \\ 0) do
-    if attempts > 3 do
-      {:error, index, event, published_event_at(index)}
-    else
-      :timer.sleep(50)
-      if event == published_event_at(index) do
-        {:ok, index, event}
-      else
-        wait_for_expected_event(event, index, attempts + 1)
-      end
-    end
-  end
-
-  defp published_event_at(index) do
-    Enum.at(Events.Persistence.published, index)
-  end
-
-  defp event_unexpected?(result) do
-    case result do
-      {:ok, _, _} ->
-        false
-      _ ->
-        true
-    end
+  defp external_event?(event) do
+    Enum.member?(@events_published_externally, event.__struct__)
   end
 end
