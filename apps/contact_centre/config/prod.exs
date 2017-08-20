2a2f436633964781e67c6700086632a9b099aed3
Joshua Fleck <joshua.fleck@simplybusiness.co.uk>
2017-08-20 11:13:16 +0100

    Contact centre pulls secret key base at runtime in prod

--- apps/contact_centre/config/prod.exs
+++ apps/contact_centre/config/prod.exs
@@ -15,7 +15,7 @@ config :contact_centre, ContactCentreWeb.Endpoint,
   http: [port: 5000],
   url: [host: "localhost", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  secret_key_base: System.get_env("SECRET_KEY_BASE")
+  secret_key_base: {:system, "SECRET_KEY_BASE"}
 
 # Do not print debug messages in production
 config :logger, level: :info
